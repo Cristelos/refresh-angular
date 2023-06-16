ef0e68d3f9f2788a8043c85aa4b27c41927bd6c2
Cristelos <cristelos@msn.com>
2023-06-16 17:37:32 +0200

    adding ngOptimizedImage for a better load of the image

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,13 @@
+import { Product } from './../../models/product.model';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+  @Input() product! : Product ;
+
+}
